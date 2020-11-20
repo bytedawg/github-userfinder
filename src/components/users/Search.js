ba5941111127d0f3edeb0b5a601ccabdd6133c47
Joshua S <jforceweb@yahoo.com>
2020-11-20 17:39:24 +1030

    search

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,30 +1,44 @@
-import React, {Component} from 'react';
-import {Form}  from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Form } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
 import { Button } from 'react-bootstrap';
 
 class Search extends Component {
-  render () {
+  state = {
+    text: ''
+  };
+
+  onChange = e => this.setState({ text: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState.text = '';
+  }
+
+
+
+  render() {
     return (
 
       <Container>
-  <Row>
-    <Col md={{ span: 4, offset: 4 }}>
-        <Form>
-          <Form.Group controlId="formBasicSearch">
-            <Form.Label>Search</Form.Label>
-            <Form.Control type="input" placeholder="Search" />
-          </Form.Group>
+        <Row>
+          <Col md={{ span: 4, offset: 4 }}>
+            <Form onSubmit={this.onSubmit}>
+              <Form.Group controlId="formBasicSearch">
+                <Form.Label>Search</Form.Label>
+                <Form.Control type="input" placeholder="Search" onChange={this.onChange} value={this.state.text} />
+              </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Search
+              <Button variant="primary" type="text" >
+                Search
           </Button>
-        </Form></Col>
-  </Row>
+            </Form></Col>
+        </Row>
 
-</Container>
+      </Container>
     )
   }
 }
